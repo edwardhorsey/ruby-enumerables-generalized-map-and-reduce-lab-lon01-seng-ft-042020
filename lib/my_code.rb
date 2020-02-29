bfb70931c80889ab28468cb5de0e7ce30f3e0b24
Edward Horsey <edwardhorsey@me.com>
2020-02-29 14:26:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,70 @@
-# Your Code Here
+
+def map_to_negativize(source_array)
+index=0
+newarray=[]
+while index < source_array.count do
+      newarray[index] = source_array[index] * -1 
+      index += 1
+    end
+return newarray
+end
+
+def map_to_no_change(source_array)
+newarray = source_array
+return newarray
+end
+
+def map_to_double(source_array)
+    index=0
+    newarray = []
+    while index < source_array.count do
+          newarray[index] = source_array[index] * 2
+          index += 1
+        end
+    return newarray
+end
+
+def map_to_square(source_array)
+    index=0
+    newarray = []
+    while index < source_array.count do
+          newarray[index] = source_array[index] ** 2 
+          index += 1
+        end
+    return newarray
+end
+
+
+def reduce_to_total(source_array, starting_point = 0)
+    index=0
+    total=0
+        while index<source_array.count do
+        total += source_array[index]
+        index+=1
+        end
+    return total + starting_point
+    end
+
+
+def reduce_to_all_true(source_array)
+index=0
+while index<source_array.count do
+    if source_array[index] == !true
+        return false
+    end
+index+=1
+end
+true
+end
+
+
+def reduce_to_any_true(source_array)
+    index=0
+    while index<source_array.count do
+        if source_array[index] == true
+            return true
+        end
+    index+=1
+    end
+false
+end
