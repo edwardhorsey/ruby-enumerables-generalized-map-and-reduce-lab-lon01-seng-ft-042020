4a442ed12153c4b16a9579760074da4b62ca6e8d
Edward Horsey <edwardhorsey@me.com>
2020-02-29 14:27:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,70 +1,17 @@
-
 def map_to_negativize(source_array)
-index=0
-newarray=[]
-while index < source_array.count do
-      newarray[index] = source_array[index] * -1 
-      index += 1
-    end
-return newarray
-end
-
-def map_to_no_change(source_array)
-newarray = source_array
-return newarray
-end
-
-def map_to_double(source_array)
     index=0
-    newarray = []
+    newarray=[]
     while index < source_array.count do
-          newarray[index] = source_array[index] * 2
+        x = source_array[index]
+        yield(x, newarray) 
           index += 1
         end
-    return newarray
-end
-
-def map_to_square(source_array)
-    index=0
-    newarray = []
-    while index < source_array.count do
-          newarray[index] = source_array[index] ** 2 
-          index += 1
-        end
-    return newarray
-end
-
-
-def reduce_to_total(source_array, starting_point = 0)
-    index=0
-    total=0
-        while index<source_array.count do
-        total += source_array[index]
-        index+=1
-        end
-    return total + starting_point
+        p newarray
+        return newarray
     end
 
+array=[1,2,3,4]
 
-def reduce_to_all_true(source_array)
-index=0
-while index<source_array.count do
-    if source_array[index] == !true
-        return false
-    end
-index+=1
-end
-true
-end
-
-
-def reduce_to_any_true(source_array)
-    index=0
-    while index<source_array.count do
-        if source_array[index] == true
-            return true
-        end
-    index+=1
-    end
-false
-end
+map_to_negativize(array) do |x, newarray|
+     newarray << x = x *-1
+end 
