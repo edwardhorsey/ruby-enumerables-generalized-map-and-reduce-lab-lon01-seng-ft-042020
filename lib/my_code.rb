6e2fdbcc51d7bd24b0aac036e3d5461e1825cc7b
Edward Horsey <edwardhorsey@me.com>
2020-02-29 15:13:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,8 @@ def reduce(source_array, starting_point=nil)
         total = starting_point
         index=0
     else
-        total = 0
-        index = 0
+        total = source_array[0]
+        index = 1
     end
 
     while index<source_array.count do
